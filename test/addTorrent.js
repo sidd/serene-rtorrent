4011c342e48343ab7b62cb8d02005e74b74c2979
Sidd Sridharan <sidd@sidd.com>
2015-12-26 13:06:03 -0800

    fixed params misalignment, added some more fields

--- test/addTorrent.js
+++ test/addTorrent.js
@@ -1,7 +1,7 @@
 var addTorrent = require('../lib/client').addTorrent
 var fs = require('fs')
 
-fs.readFile(__dirname + '/ubuntu.torrent', function (err, data) {
+fs.readFile(__dirname + '/xubuntu.torrent', function (err, data) {
   data = new Buffer(data).toString('base64')
   if (err) console.error(err)
   addTorrent(data).catch(e => console.error(e)).then(val => console.log(val))
