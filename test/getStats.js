94ff4e3a346e9fb90217309fd3547850da75ff28
Sidd Sridharan <sidd@sidd.com>
2016-01-16 00:01:47 -0800

    refactor + some tests

--- test/getStats.js
+++
@@ -1,5 +0,0 @@
-var Client = require('../lib/client')({
-  host: 'http://localhost:8080/RPC2'
-})
-
-Client.getStats().catch(err => console.error(err)).then(res => console.log(res))
--- test/removeTorrent.js
+++
@@ -1,3 +0,0 @@
-var removeTorrent = require('../lib/client').removeTorrent
-
-removeTorrent('4CB67059ED6BD08362DA625B3AE77F6F4A075705', 'start').catch(err => console.error(err)).then(res => console.log(res))
